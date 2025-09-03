501fdb2abf9d8de6761091d544ee9d1f0edf5f95
devaasoni <sonidev774@gmail.com>
2025-09-03 20:32:41 +0530

    added responsiveness & updated service's animation

--- aim_wise_networking/static/script.js
+++ aim_wise_networking/static/script.js
@@ -355,8 +355,8 @@ document.addEventListener("DOMContentLoaded", () => {
 window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   if (window.scrollY > 50) {
-    header.classList.add("scrolled");
+    header.classList.add("shrink");
   } else {
-    header.classList.remove("scrolled");
+    header.classList.remove("shrink");
   }
 });
